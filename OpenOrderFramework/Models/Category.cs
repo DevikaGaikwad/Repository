d054673ec365ccecb9a42dcc442af029e70d65a5
Mufaddal Gulshan <mufaddal.gulshan@gmail.com>
2016-03-03 14:51:17 +0530

    - Fixed wrong commit

---
+++ OpenOrderFramework/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace OpenOrderFramework.Models
+{
+    public class Category
+    {
+        [Key]
+        [DisplayName("Category ID")]
+        public int ID { get; set; }
+
+        [DisplayName("Category")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
